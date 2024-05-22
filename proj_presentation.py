df8fc69f0ffd1fd25e69e1f810d5bb58b09d6544
Aramiles <136480153+Aramiles@users.noreply.github.com>
2024-05-22 22:10:39 +0100

    streamlit

---
+++ proj_presentation.py
@@ -0,0 +1,224 @@
+import streamlit as st
+from projet_info import molecule
+from rdkit import Chem
+from rdkit.Chem import Draw
+import os
+import pandas as pd
+import re
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+
+def wiki_search(name):
+    firefox_driver_path = "geckodriver-v0.33.0-win64/geckodriver.exe"
+    firefox_options = Options()
+    firefox_options.add_argument('--headless')  # Activer le mode headless
+    firefox_options.add_argument('--window-size=800,800')
+    
+    service = Service(executable_path=firefox_driver_path)
+    browser = webdriver.Firefox(service=service, options=firefox_options)
+    browser.get("https://en.wikipedia.org/wiki/Main_Page")
+    
+    # Localiser la zone de recherche et saisir le texte
+    search_box = browser.find_element(By.ID, 'searchInput')
+    search_box.send_keys(name)
+    search_box.submit()
+    import time
+    time.sleep(1)
+    
+    paragraphs = browser.find_elements(By.TAG_NAME, 'p')
+    first_paragraph_with_text = None
+    for paragraph in paragraphs:
+        if paragraph.text.strip() != "":
+            first_paragraph_with_text = paragraph
+            break
+    
+    clean_text = re.sub(r'\[\d+\]', '', first_paragraph_with_text.text)
+    st.write(clean_text)
+    st.write(f"from the english wikipedia page about {name}")
+    time.sleep(1)
+    browser.quit()
+
+            #title
+
+
+st.title("project of molecule's analysis")
+
+st.subheader("1)   **let's charge datas**")
+
+            #charge datas
+
+user_input = st.text_input("write the name of your folder on this case")
+try:
+    new_mol = molecule(user_input)
+    if new_mol.load_verify_st() == True :
+        mole= molecule(user_input)
+        # Afficher la saisie de l'utilisateur
+        st.write("You want to analyse the molecule: ", user_input[:-4])
+        number_of_atoms = len(mole.get_atoms)
+
+        button_clicked_mol = st.button("small description")
+        if button_clicked_mol:
+            wiki_search(user_input[:-4])
+
+
+        
+        
+        col1, col2 = st.columns(2)
+
+           #show molecules
+
+        
+        with col1:
+            button_clicked_mol = st.button("show molecule")
+            if button_clicked_mol:
+                img_mole = Draw.MolToImage(Chem.MolFromMolFile(os.path.join(os.path.dirname(os.path.abspath(__file__))+"\MOL_files", user_input)))
+                st.image(img_mole, caption="Molecular structure", width=300)     
+                
+        with col2:
+                st.table(pd.DataFrame(mole.get_infos.values(), index = mole.get_infos.keys(), columns = ["datas"]))
+        
+        
+        st.markdown("""<style>.centered-line {width: 90%;margin: 0 auto;border: 1px solid #ddd}</style>""", unsafe_allow_html=True)
+        st.markdown("<hr class='centered-line'>", unsafe_allow_html=True)
+
+        
+        col1, col2 = st.columns(2)
+
+
+        
+        # show .mol file
+
+        
+        with col1:
+            st.subheader("about atoms")
+            button_clicked_atom = st.button("show atom's bloc")
+        
+        if button_clicked_atom:
+            st.table(mole.get_atoms)
+        
+
+        
+        with col2:
+            st.subheader("about bonds")
+            button_clicked_bonds = st.button("show bond's bloc")
+        
+            # Afficher le tableau si le bouton est cliqué
+        if button_clicked_bonds:
+            st.table(mole.get_bonds)
+        
+        st.markdown("""<style>.centered-line {width: 90%;margin: 0 auto;border: 1px solid #ddd}</style>""", unsafe_allow_html=True)
+        st.markdown("<hr class='centered-line'>", unsafe_allow_html=True)
+        
+        st.subheader("3)   **Some functionalities**")
+
+
+            #project's functions
+
+        
+        col1, col2 = st.columns(2)
+
+
+
+            #count elements
+        
+        with col1:
+            st.subheader("A- count elements")
+            st.subheader("")
+            element = st.text_input("Please enter the type of atom by its letter")
+            if mole.count_elements(element) != 0:
+                st.success(f"The number of {element} atom elements in the molecule is {mole.count_elements(element)}.") 
+            elif element == "":
+                st.warning("No element provided")
+            else:
+                st.warning(f"The atom element {element} does not exist in the molecule.")
+
+
+
+                 #2D distance
+            
+            st.subheader("C- Topological distance between two atoms of the molecule")
+
+            couple = st.text_input(f"Please enter the identifier of atom 1 and 2 with a space between (between 1 and {number_of_atoms}) ")
+            
+            try:
+                
+                atom1_top, atom2_top = map(int, couple.split())
+                
+                if atom1_top == atom2_top:
+                    st.success(f"The distance between the atoms {atom1_top} and {atom2_top} in the molecule is 0.")
+                elif (1 <= atom1_top <= number_of_atoms) and (1 <= atom2_top <= number_of_atoms):
+                    distance = mole.distance_topo_2D(atom1_top, atom2_top)
+                    st.success(f"The distance between the atoms {atom1_top} and {atom2_top} in the molecule is {distance}.")
+                else:
+                    st.warning(f"One or both of the atoms {atom1_top} and {atom2_top} do not exist in the molecule.")
+            except ValueError:
+                st.warning("Please enter two valid atom identifiers separated by a space.")
+            except Exception as e:
+                st.warning(f"An error occurred: {e}")
+
+
+                #3D distance
+
+        
+        with col2:
+            
+            # 3D distance
+            st.subheader("B- 3D Distance Between Two Atoms of the Molecule")
+            couple3D = st.text_input(f"Please enter the identifier of atom 1 and 2 with a space between (between 1 and {number_of_atoms})")
+            
+            try:
+                atom1_3D, atom2_3D = map(int, couple3D.split())
+                if atom1_3D == atom2_3D:
+                    st.success(f"The distance between the atoms {atom1_3D} and {atom2_3D} in the molecule is 0.")
+                elif 1 <= atom1_3D <= number_of_atoms and 1 <= atom2_3D <= number_of_atoms:
+                    distance3D = mole.distance_3D(atom1_3D, atom2_3D)
+                    st.success(f"The distance between the atoms {atom1_3D} and {atom2_3D} in the molecule is {distance3D}.")
+                else:
+                    st.warning(f"One or both of the atoms {atom1_3D} and {atom2_3D} do not exist in the molecule.")
+            except ValueError:
+                st.warning("Please enter two valid atom identifiers separated by a space.")
+            except Exception as e:
+                st.warning(f"An error occurred: {e}")
+        
+            # Neighbours
+            st.subheader("D- Atom's Neighbours")
+            ato = st.text_input(f"Please enter the identifier of an atom (between 1 and {number_of_atoms})")
+            
+            if ato.strip() == "":
+                st.warning("No atom provided")
+            else:
+                try:
+                    ato = int(ato)
+                    if 1 <= ato <= number_of_atoms:
+                        neighbours = mole.atom_neighbours(ato)
+                        if neighbours:
+                            st.success(f"The number of neighbours for atom {ato} is {len(neighbours)}.")
+                        else:
+                            st.warning(f"The atom {ato} has no neighbours.")
+                    else:
+                        st.warning(f"The atom {ato} does not exist in the molecule.")
+                except ValueError:
+                    st.warning("Please enter a valid integer for the atom identifier.")
+                except Exception as e:
+                    st.warning(f"An error occurred: {e}")
+
+
+                #ring
+                    
+        st.subheader("E- Ring verification")
+        st.subheader("")
+        ring_chain = st.text_input(f"Please enter the identifiers of atoms you want to check if they form a ring. Put an espace between each indentifier (identifiers should be between 1 and {number_of_atoms}) ")
+        mole.ring_finding_st(ring_chain)
+
+                
+except:
+    st.write("File not found or no file provided")
+
+
+#lydia Aly (diplomate egyptienne en france)
+#Sara Elessawy (diplomate egyptienne en france)
+#Menna-tullah Moustafa (diplomate egyptienne au caire)
+#Ehab Ahmed Badawy (ambassadeur d'Egypte en france)
---
+++ projet_info.py
@@ -0,0 +1,108 @@
+import pandas as pd
+import os
+import streamlit as st
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+organic_atoms = ['H', 'B', 'C', 'N', 'O', 'P', 'S', 'F', 'Cl', 'Br', 'I']
+
+#to reduce the dimension of a list
+
+def flat_list(list):
+    flat = []
+    [flat.extend(mini_list) for mini_list in list]
+    return flat
+
+#class for each molecule
+
+class molecule:
+    def __init__(self, fichier_name):
+        self.fichier = open(os.path.join(os.path.dirname(os.path.abspath(__file__))+"\MOL_files", fichier_name),"r").readlines()         #read the file 
+        self.fichier_name = fichier_name
+
+        #overall informations found in the head of the .mol file
+        self.get_infos = {'brut formula':self.fichier[0][:-1], 'nb of atoms':int(self.fichier[3][:-1].split()[0]), 'nb of bonds':int(self.fichier[3][:-1].split()[1])}
+        list_fichier = [k[:-1].split() for k in self.fichier[4:]]
+        
+        #atom bloc where we can find informations about atoms in a dataframe 
+        self.get_atoms = pd.DataFrame([k for k in list_fichier if len(k)==len(list_fichier[0])], #atom bloc begins at line 4. We take all the line of the same lenght in the file
+                                      index = [k+1 for k in range (len([k for k in list_fichier if len(k)==len(list_fichier[0])]))], #index corresponds to atom number, it begins at 1
+                                      columns = ['x', 'y', 'z','atom', 'mass difference', 'charge', 'atom stereo parity']+
+                                      [k for k in range (len([k[:-1].split() for k in self.fichier[4:] if len(k)==len(self.fichier[4])][0])-7)])       #we don't know the title of the other columns
+
+        # bond bloc where we can find informations about bonds in a dataframe 
+        self.get_bonds = pd.DataFrame([k[:-1].split() for k in self.fichier[4:-2] if len(k[:-1].split())==7],        #As there is 7 features to describe a bond, we take the lines of lenght 7 of the file
+                                      columns = ['first atom nb','second atom nb', 'order', 'stereo','useless','topology', 'reacting center status'])
+
+        
+
+    def load_verify(self):
+        if all(atom in organic_atoms for atom in self.get_atoms['atom']) is False:           #we verify that all the atoms in the atom_bloc are in the list of the organic atoms
+            print(f"File {self.fichier_name} could not be loaded. Contains non-organic atom elements.")
+            return False
+        elif len(self.get_bonds) != int(self.get_infos['nb of bonds']):                #we  verify that lenght of the bond bloc corresponds to the number given in the head bloc
+            print(f'File {self.fichier_name} could not be loaded. Bonds block is incomplete.')
+            return False
+        elif len(set(list(self.get_bonds['first atom nb'])+list(self.get_bonds['second atom nb']))) != len(self.get_atoms): #we  verify that all the atoms are named somewhere in the bond bloc
+            print(f'File {self.fichier_name} could not be loaded. Bonds block does not cover all atoms.')
+            return False
+        else:
+            print(f"File {self.fichier_name} validated and loaded!")
+            return True
+
+
+
+    def load_verify_st(self):
+        if all(atom in organic_atoms for atom in self.get_atoms['atom']) is False:           #we verify that all the atoms in the atom_bloc are in the list of the organic atoms
+            st.write(f"File {self.fichier_name} could not be loaded. Contains non-organic atom elements.")
+            return False
+        elif len(self.get_bonds) != int(self.get_infos['nb of bonds']):                #we  verify that lenght of the bond bloc corresponds to the number given in the head bloc
+            st.write(f'File {self.fichier_name} could not be loaded. Bonds block is incomplete.')
+            return False
+        elif len(set(list(self.get_bonds['first atom nb'])+list(self.get_bonds['second atom nb']))) != len(self.get_atoms): #we  verify that all the atoms are named somewhere in the bond bloc
+            st.write(f'File {self.fichier_name} could not be loaded. Bonds block does not cover all atoms.')
+            return False
+        else:
+            st.write(f"File {self.fichier_name} validated and loaded!")
+            return True
+
+    def count_elements(self, element):
+        return len([k for k in list(self.get_atoms['atom']) if k == element])   #we add a string in the list each time the letter of the element desired is named. the lenght give us the number of atoms of the given element. 
+
+
+    def distance_3D(self, atom1, atom2):
+        return round(((float(self.get_atoms['x'][atom1])-float(self.get_atoms['x'][atom2]))**2 + (float(self.get_atoms['y'][atom1])-float(self.get_atoms['y'][atom2]))**2 + (float(self.get_atoms['z'][atom1])-float(self.get_atoms['z'][atom2]))**2)**(1/2),2)                #calcul of the distance
+        
+
+    def atom_neighbours(self, atom_identifier):
+        return [int(self.get_bonds['second atom nb'][i]) for i, atoms in enumerate(self.get_bonds['first atom nb']) if int(atoms) == atom_identifier]+[int(self.get_bonds['first atom nb'][i]) for i, atoms in enumerate(self.get_bonds['second atom nb']) if int(atoms) == atom_identifier]      #we put in a list the second atom's identifier each time the targeted atom is seen in the 'atom' column of the atom bloc. We'll return this list because it can be useful for the topological distance between the atoms 
+
+
+    def distance_topo_2D(self, atom1, atom2):
+        neighbours_path = self.atom_neighbours(atom1)   #As mentioned, we use the method above to find the first circle of atoms around the atom1
+        k = 1
+        while atom2 not in neighbours_path:   #We increase the diameter of the circle until the circle meet the atom2. Meaning that, for each atom in the neighbours_path's list, we put there neighbours in the list, we increase the k counter and we check that atom2 is not in the list. We repeat this until he is.
+            neighbours_path += flat_list([self.atom_neighbours(neighbour) for neighbour in neighbours_path])
+            k += 1
+        return k
+
+    def ring_finding(self, ring_path):
+        bond_couple = [(self.get_bonds['first atom nb'][k],self.get_bonds['second atom nb'][k]) for k in range(len(self.get_bonds))]+[(self.get_bonds['second atom nb'][k], self.get_bonds['first atom nb'][k]) for k in range(len(self.get_bonds))]     #We create a list of tuple. Each tuple represent a bond. We add the tuple in both direction : (3,4) and (4,3).
+        if len(ring_path) > 2 and ring_path[0] == ring_path[-1] and all(bond in bond_couple for bond in [(ring_path[k], ring_path[k+1]) for k in range(len(ring_path[:-1]))]) is True:    #we check that all the bond (tuple) between the atoms of the ring path are in our bond_couple list. 
+            print("Path exists.")
+        else:
+            print("Path does not exist.")
+
+    def ring_finding_st(self, ring_path):
+        bond_couple = [(self.get_bonds['first atom nb'][k],self.get_bonds['second atom nb'][k]) for k in range(len(self.get_bonds))]+[(self.get_bonds['second atom nb'][k], self.get_bonds['first atom nb'][k]) for k in range(len(self.get_bonds))]     #We create a list of tuple. Each tuple represent a bond. We add the tuple in both direction : (3,4) and (4,3).
+        if len(ring_path) > 2 and ring_path[0] == ring_path[-1] and all(bond in bond_couple for bond in [(ring_path[k], ring_path[k+1]) for k in range(len(ring_path[:-1]))]) is True:    #we check that all the bond (tuple) between the atoms of the ring path are in our bond_couple list. 
+            st.success("Path exists.")
+        else:
+            st.error("Path does not exist.")
+
+#This class will be useful for subsequents methods comparing molecules
+class all_molecule:
+    def __init__(self):
+        self.molecules = {}
+
+    def add_molecule(self, molecule, name):
+        self.molecules[name] = {'overall_info':molecule.get_infos, 'atom_bloc' :molecule.get_atoms, 'bond_bloc' : molecule.get_bonds}
+
